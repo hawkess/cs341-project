b0bebb552cef683add5fbd51859f1b79f6894440
hawkess <50639790+hawkess@users.noreply.github.com>
2020-05-25 15:45:16 -0500

    Rearranged article view

--- web/view.php
+++ web/view.php
@@ -3,8 +3,8 @@
    <?php include "include/navbar.php" ?>
     <div class="container-fluid">
         <h1 class="w-75"><?php echo htmlspecialchars($results['article']->title)?></h1>
-        <div class="w-75"><?php echo $results['article']->content?></div>
         <p class="created">Published on <?php echo date('j F Y', $results['article']->created)?></p>
+        <div class="w-75"><?php echo $results['article']->content?></div>        
     </div>
 </body>
 </html>
