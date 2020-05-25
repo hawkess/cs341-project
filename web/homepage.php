b8cb5dcd0d962217658f363ab525194745a81325
hawkess <50639790+hawkess@users.noreply.github.com>
2020-05-25 07:56:31 -0500

    More timestamp nonsense

--- web/homepage.php
+++ web/homepage.php
@@ -6,7 +6,7 @@
             <?php foreach ($results['articles'] as $article) { ?>
             <li>
                 <h2>
-                    <span class="created"><?php echo date('j F', $article->created)?></span><a href=".?action=viewArticle&amp;articleId=<?php echo $article->id?>"><?php echo htmlspecialchars($article->title)?></a>
+                    <span class="created"><?php echo date('j F', $article->created)?></span><a href="index.php?action=viewArticle&amp;articleId=<?php echo $article->id?>"><?php echo htmlspecialchars($article->title)?></a>
                 </h2>
                 <p class="summary"><?php echo htmlspecialchars(substr($article->content, 0, 100))?></p>
             </li>
