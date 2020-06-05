082025308b8cc05b62d6b843afef14d6cae37a36
hawkess <50639790+hawkess@users.noreply.github.com>
2020-06-05 18:30:52 -0500

    editArticle form restyled

--- web/admin/editArticle.php
+++ web/admin/editArticle.php
@@ -1,32 +1,34 @@
 <?php include "include/header.php" ?>
-     <body>
-     <?php include "include/navbar.php" ?>
-      <div id="adminHeader">
+
+<body>
+    <?php include "include/navbar.php" ?>
+    <div id="adminHeader">
         <h2>CSE 341 CMS Admin</h2>
-      </div>
-      <h1><?php echo $results['pageTitle']?></h1>
-      <form action="admin.php?action=<?php echo $results['formAction']?>" method="post">
-        <input type="hidden" name="articleId" value="<?php echo $results['article']->id ?>"/>
-<?php if (isset($results['errorMessage'])) { ?>
+    </div>
+    <h1><?php echo $results['pageTitle']?></h1>
+    <div class="container-fluid">
+    <form action="admin.php?action=<?php echo $results['formAction']?>" method="post">
+        <input type="hidden" name="articleId" value="<?php echo $results['article']->id ?>" />
+        <?php if (isset($results['errorMessage'])) { ?>
         <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
-<?php } ?>
-        <ul>
-          <li>
+        <?php } ?>
+        <div class="form-group">
             <label for="title">Article Title</label>
-            <input type="text" name="title" id="title" placeholder="Name of the article" required autofocus maxlength="255" value="<?php echo htmlspecialchars($results['article']->title)?>" />
-          </li>
-          <li>
-            <label for="content">Article Content</label>
-            <textarea name="content" id="content" placeholder="The content of the article" required maxlength="100000" style="height: 30em;"><?php echo htmlspecialchars($results['article']->content)?></textarea>
-          </li>
-        </ul>
-        <div class="buttons">
-          <input type="submit" name="saveChanges" value="Save Changes" />
-          <input type="submit" formnovalidate name="cancel" value="Cancel" />
+            <input type="text" class="form-control" name="title" id="title" placeholder="Name of the article" required autofocus maxlength="255" value="<?php echo htmlspecialchars($results['article']->title)?>" />
         </div>
-      </form>
-<?php if ($results['article']->id) { ?>
-      <p><a href="admin.php?action=deleteArticle&amp;articleId=<?php echo $results['article']->id ?>" onclick="return confirm('Delete This Article?')">Delete This Article</a></p>
-<?php } ?>
+        <div class="form-group">
+            <label for="content">Article Content</label>
+            <textarea class="form-control" name="content" id="content" placeholder="The content of the article" required maxlength="100000" rows="5"><?php echo htmlspecialchars($results['article']->content)?></textarea>
+        </div>
+        <div class="buttons">
+            <input type="submit" name="saveChanges" value="Save Changes" />
+            <input type="submit" formnovalidate name="cancel" value="Cancel" />
+        </div>
+    </form>
+    <?php if ($results['article']->id) { ?>
+    <a href="admin.php?action=deleteArticle&amp;articleId=<?php echo $results['article']->id ?>" onclick="return confirm('Delete This Article?')">Delete This Article</a>
+    <?php } ?>
+    </div>
 </body>
+
 </html>
