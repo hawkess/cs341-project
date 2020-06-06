b05d31de4a16c08094f245520c5b94a484f9436b
hawkess <50639790+hawkess@users.noreply.github.com>
2020-06-06 06:47:55 -0500

    Continuing style changes

--- web/welcome.php
+++ web/welcome.php
@@ -12,13 +12,13 @@ $results['pageTitle'] = "CSE 341 Welcome Page";
     <?php include "include/navbar.php" ?>
     <div class="container-fluid">
         <div class="w-25 m-auto py-5">
-            <div class="page-header">
+            <div class="page-header pb-4">
                 <h2>Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
             </div>
             <div class="list-group">
                <a href="admin.php" class="list-group-item list-group-item-action"><i class="fas fa-cog pr-2"></i>Manage Articles</a>
                 <a href="admin.php?action=resetpassword" class="list-group-item list-group-item-action list-group-item-warning"><i class="fas fa-unlock-alt pr-2"></i>Reset Password</a>
-                <a href="admin.php?action=logout" class="list-group-item list-group-item-action list-group-item-secondary"><i class="fas fa-power-off pr-2"></i>Sign Out</a>
+                <a href="admin.php?action=logout" class="list-group-item list-group-item-action list-group-item-dark"><i class="fas fa-power-off pr-2"></i>Sign Out</a>
             </p>
         </div>
     </div>
