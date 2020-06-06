de55b6babf5edccffec6aa2309c0d236b815acde
hawkess <50639790+hawkess@users.noreply.github.com>
2020-06-06 04:29:23 -0500

    Continuing style changes

--- web/view.php
+++ web/view.php
@@ -3,10 +3,11 @@
 <body>
     <?php include "include/navbar.php" ?>
     <div class="container-fluid d-flex justify-content-center">
-        <div class="w-25 m-auto py-5" <div class="container">
+        <div class="w-25 m-auto py-5">
+           <div class="container">
             <h3><?php echo htmlspecialchars($results['article']->title)?></h3>
-            <h5 id="author"><?php echo $results['article']->author?></h5>
-            <h5 id="created"><?php echo date('F j\, Y', $results['article']->created)?></h5>
+            <p><?php echo $results['article']->author?></p>
+            <p><?php echo date('F j\, Y', $results['article']->created)?></p>
             <div><?php echo $results['article']->content?></div>
         </div>
         <?php
@@ -14,10 +15,9 @@
         {
             if ($results['article']->user_id == $_SESSION["user_id"])
             {
-                echo '<div class="w-75">
+                echo '<div class="buttons">
                 <a type="button" class="btn btn-primary" href="admin.php?action=editArticle&articleId=' . $results['article']->id . '"><i class="fas fa-pen pr-2"></i>Edit</button>
-                <a type="button" class="btn btn-danger" href="admin.php?action=deleteArticle&articleId=' . $results['article']->id . '"><i class="fa fa-trash-o pr-2"></i>Delete</button>
-                </div>';
+                <a type="button" class="btn btn-danger" href="admin.php?action=deleteArticle&articleId=' . $results['article']->id . '"><i class="fa fa-trash-o pr-2"></i>Delete</button></div>';
             }
         }
         ?>
